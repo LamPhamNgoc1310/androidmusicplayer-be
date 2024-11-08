3c3152d5d4ab03bdffeb0219925e38f4e6e0dfc8
LamPhamNgoc1310 <phamngoclam1310@gmail.com>
2024-11-09 00:49:07 +0700

    docs: add public folder

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const path = require("path")
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "car.jpg"));
+  });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
