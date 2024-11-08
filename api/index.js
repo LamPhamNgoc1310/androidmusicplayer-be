c99c6e1f4eb785d1efd2af8cc3b29140f3fb1b71
LamPhamNgoc1310 <phamngoclam1310@gmail.com>
2024-11-09 01:07:14 +0700

    docs: add a song

--- api/index.js
+++ api/index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const path = require("path")
 const app = express();
 
+app.use('/audio', express.static(path.join(__dirname, 'songs')));
+
+app.get('/play', (req, res) => {
+    res.sendFile(path.join(__dirname, 'songs', 'song1.mp3'));
+  
+  });
+  
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "car.jpg"));
   });
